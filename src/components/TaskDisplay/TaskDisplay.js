25ff423e5bfec0cd600d546afd0c06652b12dc39
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-29 15:50:29 -0700

    debugging subtask sorting

--- src/components/TaskDisplay/TaskDisplay.js
+++ src/components/TaskDisplay/TaskDisplay.js
@@ -2,30 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default () => {
-    const dummyData = [
-        {
-            title: "do task 1",
-            completed: false,
-            created_at: 0,
-            updated_at: 1,
-            scheduled_at: 10
-        }
-    ];
-    const [subTasks, setSubTasks] = useState(dummyData);
+    const [subtasks, setSubtasks] = useState([]);
 
-    // useEffect(() => {
-    //     const key = '9ed169d0'
-    //     const headers = { "X-API-Key": key }
-    //     axios
-    //         .get('https://my.api.mockaroo.com/todos.json', { headers })
-    //         .then(console.log)
-    //         .catch(console.log);
-    // }, []);
+    function sortedSubtasks(unsorted) {
+        function compareSubtasks(a, b) {
+            // console.log(a.completed, b.completed, +a.completed > +b.completed)
+            if (+a.completed > +b.completed) {
+                return -1;
+            } else if (+a.completed > +b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        console.log([...unsorted].sort(compareSubtasks).map(t => t.completed))
+
+        return [...unsorted].sort(compareSubtasks);
+    }
+
+    useEffect(() => {
+        const key = '9ed169d0'
+        const headers = { "X-API-Key": key }
+        axios
+            .get('https://my.api.mockaroo.com/todos.json', { headers })
+            .then(res => {
+                setSubtasks(sortedSubtasks(res.data));
+            })
+            .catch(console.log);
+    }, []);
 
     return (
         <div>
             <h1>hello world</h1>
-            {subTasks.map(task => (<li>{task.title}</li>))}
+            {subtasks && subtasks.map(task => (<li>{task.title.concat(task.completed ? " COMPLETED" : " PENDING")}</li>))}
         </div>
     )
 }
