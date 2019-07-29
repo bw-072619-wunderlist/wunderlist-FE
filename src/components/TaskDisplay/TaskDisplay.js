bfafd0a04d6624b10e6e9fd41d82cce84fe1db04
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-29 15:05:42 -0700

    axios get for dummy data

--- src/components/TaskDisplay/TaskDisplay.js
+++ src/components/TaskDisplay/TaskDisplay.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default () => {
     const dummyData = [
@@ -11,11 +12,20 @@ export default () => {
         }
     ];
     const [subTasks, setSubTasks] = useState(dummyData);
-  
-  return(
-    <div>
-      <h1>hello world</h1>
-      {subTasks.map(task => (<li>{task.title}</li>))}
-    </div>
-  )
+
+    // useEffect(() => {
+    //     const key = '9ed169d0'
+    //     const headers = { "X-API-Key": key }
+    //     axios
+    //         .get('https://my.api.mockaroo.com/todos.json', { headers })
+    //         .then(console.log)
+    //         .catch(console.log);
+    // }, []);
+
+    return (
+        <div>
+            <h1>hello world</h1>
+            {subTasks.map(task => (<li>{task.title}</li>))}
+        </div>
+    )
 }
