df8dfd8d6807db6cbff80951872c9cbdba1005e9
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-29 13:30:54 -0700

    install dependencies; quotation and semicolon style corrections; TaskDisplay displays at all

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Route, Link, Redirect } from 'react-router-dom';
 
 import './App.scss';
-import Home from './components/Home/Home'
-import Login from './components/Login/Login'
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import TaskDisplay from './components/TaskDisplay/TaskDisplay';
 
 function App() {
   return (
-    <div className="app-container">
-      <header className="app-header">
+    <div className='app-container'>
+      <header className='app-header'>
         TASK App
       </header>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route path='/task/:id' component={TaskDisplay} />
     </div>
   );
 }
