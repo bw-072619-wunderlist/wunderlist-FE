8eb9216ee84c8c0974e71ece8c4e72504d5c438d
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-29 14:03:10 -0700

    displays subtasks; WIP: custom hook

--- src/components/TaskDisplay/TaskDisplay.js
+++ src/components/TaskDisplay/TaskDisplay.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default () => {
+    const dummyData = [
+        {
+            title: "do task 1",
+            completed: false,
+            created_at: 0,
+            updated_at: 1,
+            scheduled_at: 10
+        }
+    ];
+    const [subTasks, setSubTasks] = useState(dummyData);
   
   return(
     <div>
-      hello world
+      <h1>hello world</h1>
+      {subTasks.map(task => (<li>{task.title}</li>))}
     </div>
   )
 }
