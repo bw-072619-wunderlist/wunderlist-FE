b404fba8aaf4be6f2d1375badd450ae98d4013be
Dora Belme <contact.dorabelme@gmail.com>
2019-07-30 13:56:09 -0400

    added sideNav with Accordion

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Link, Redirect } from 'react-router-dom';
 import EditProfile from './components/Profile/EditProfile';
 import Profile from './components/Profile/profileCard';
+import AccordionExampleStandard from './components/Profile/SideNav';
 
 import './App.scss';
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
+
 
 function App() {
   return (
@@ -15,8 +17,10 @@ function App() {
       </header>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/profile" component={Profile}/>
-      <Route exact path="/profile/edit" component={EditProfile}/>
+      {/* <Route exact path="/profile" component={Profile} /> */}
+      <Route exact path="/profile" component={AccordionExampleStandard} />
+      <Route exact path="/profile/edit" component={EditProfile} />
+     
     </div>
   );
 }
