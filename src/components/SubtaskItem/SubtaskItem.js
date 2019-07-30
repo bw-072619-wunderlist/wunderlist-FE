e0d4f8c490d4206ef232d45d3d1a6425e0ca60c5
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-30 14:51:11 -0700

    fetch data and display it correctly

--- src/components/SubtaskItem/SubtaskItem.js
+++ src/components/SubtaskItem/SubtaskItem.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-export default ({ completed, title }) => {
+export default ({ completed, name }) => {
 
     return (
         <li>
             <div className={
                 completed ? 'checkbox checked' : 'checkbox unchecked'
-            }><i class="fas fa-check fa-sm"></i></div>
-            <p>{title}</p>
+            }><i className="fas fa-check fa-sm"></i></div>
+            <p>{name}</p>
             <div className='cal-icon'>
                 <i class="far fa-calendar fa-lg"></i>
             </div>
             <div className='del-icon'>
-                <i class="fas fa-times fa-lg"></i>
+                <i className="fas fa-times fa-lg"></i>
             </div>
         </li>
     )
--- src/components/TaskDisplay/TaskDisplay.js
+++ src/components/TaskDisplay/TaskDisplay.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 import SubtaskItem from '../SubtaskItem/SubtaskItem';
+import AxiosWithAuth from '../../utils/AxiosWithAuth'; 
 
 export default () => {
     const [subtasks, setSubtasks] = useState([]);
@@ -20,23 +20,35 @@ export default () => {
         return [...unsorted].sort(compareSubtasks);
     }
 
+    // useEffect(() => {
+    //     const key = '9ed169d0'
+    //     const headers = { "X-API-Key": key }
+    //     axios
+    //         .get('https://my.api.mockaroo.com/todos.json', { headers })
+    //         .then(res => {
+    //             const sorted = sortedSubtasks(res.data);
+    //             setSubtasks(sortedSubtasks(res.data));
+    //         })
+    //         .catch(console.log);
+    // }, []);
+
     useEffect(() => {
-        const key = '9ed169d0'
-        const headers = { "X-API-Key": key }
-        axios
-            .get('https://my.api.mockaroo.com/todos.json', { headers })
+        AxiosWithAuth()
+            .get(`https://wunderlist-be.herokuapp.com/api/v2/todos/1`)
             .then(res => {
-                const sorted = sortedSubtasks(res.data);
-                setSubtasks(sortedSubtasks(res.data));
+                console.log(res.data.tasks)
+                setSubtasks(sortedSubtasks(res.data.tasks));
             })
-            .catch(console.log);
-    }, []);
+            .catch(error => {
+              console.log(error)
+            });
+    }, []); 
 
     return (
         <div>
             <h1>hello world</h1>
             <ul>{subtasks && subtasks.map(task => (
-                <SubtaskItem completed={task.completed} title={task.title} />
+                <SubtaskItem completed={task.completed} name={task.name} />
             ))}</ul>
         </div>
     )
