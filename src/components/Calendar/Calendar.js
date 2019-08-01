e67499225f8810160ccd47ba5750604e68387406
unknown <samantha.c.egge@gmail.com>
2019-08-01 14:58:02 -0700

    Commiting minor changes to calendar to merge.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,61 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import { Calendar } from 'react-big-calendar';
-import moment from 'moment';
+// import React, { useState, useEffect } from 'react';
+// import BigCalendar from 'react-big-calendar';
+// import moment from 'moment';
 
-import AxiosWithAuth from '../../utils/AxiosWithAuth';
+// import AxiosWithAuth from '../../utils/AxiosWithAuth';
 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 // moment.locale('en-GB');
-// Calendar.momentLocalizer(moment);
+// BigCalendar.momentLocalizer(moment);
 
-const CalendarDisplay = () => {
-  const [calEvents, setCalEvents] = useState([])
+// const CalendarDisplay = () => {
+//   const [calEvents, setCalEvents] = useState([])
 
-  const convertDate = (date) => {
-    return moment.utc(date).toDate()
-  }
+//   const convertDate = (date) => {
+//     return moment.utc(date).toDate()
+//   }
 
-  useEffect(() => {
-    AxiosWithAuth()
-      .get('https://wunderlist-be.herokuapp.com/api/v2/todos')
-      .then(response => {
-        console.log(response.data);
-        let tasks = response.data;
+//   useEffect(() => {
+//     AxiosWithAuth()
+//       .get('https://wunderlist-be.herokuapp.com/api/v2/todos')
+//       .then(response => {
+//         console.log(response.data);
+//         let tasks = response.data;
 
-        for (let i = 0; i < tasks.length; i++) {
-
-          tasks[i].start = convertDate(tasks[i].start)
-          tasks[i].end = convertDate(tasks[i].end)
-          
-        }
-
-        setCalEvents({
-          calEvents:tasks
-        })
-        console.log(calEvents)
+//         for (let i = 0; i < tasks.length; i++) {
+//           tasks[i].start = convertDate(tasks[i].start)
+//           tasks[i].end = convertDate(tasks[i].end)
+//         }
+//         setCalEvents({
+//           calEvents:tasks
+//         })
+//         console.log(calEvents)
   
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  })
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   })
 
-  return (
-    <div>
-      This is a calendar
-      <div style={{ height: 700 }}>
-        <Calendar
-          events={calEvents}
-          step={30}
-          defaultView='month'
-          views={['month','week','day']}
-          defaultDate={new Date()}
-        />
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       This is a calendar
+//       <div style={{ height: 700 }}>
+//         <Calendar
+//           events={calEvents}
+//           step={30}
+//           defaultView='month'
+//           views={['month','week','day']}
+//           defaultDate={new Date()}
+//         />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default CalendarDisplay
+// export default CalendarDisplay
