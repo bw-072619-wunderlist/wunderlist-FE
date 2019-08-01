35bb423d81924769d5498936e10e9727cf797608
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-01 12:56:28 -0700

    fix: style bleeding

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,24 @@
-import React, { useEffect} from 'react'
-import AxiosWithAuth from '../../utils/AxiosWithAuth'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+// import AxiosWithAuth from '../../utils/AxiosWithAuth'
 
-const Search = (props) => {
+const Search = ({ filteredTasks, query }) => {
 
-  console.log('search props :', props)
+  console.log('search props :', filteredTasks)
 
-  return (
-    <div>
-      search page
-    </div>
-  )
+  if (filteredTasks.length > 0) {
+    return (
+      <div>
+        <ul>
+          {filteredTasks.map(task => (
+            <li><Link to={`task/${task.id}`}>{task.title}</Link></li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div><h2>No Search Results For "{query}"</h2></div>;
+  }
 }
 
 export default Search
