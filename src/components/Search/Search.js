a98f0309c54387a9e3464824e8c2e3d3692324ad
unknown <samantha.c.egge@gmail.com>
2019-08-01 12:02:40 -0700

    modified search slightly

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,27 +3,6 @@ import AxiosWithAuth from '../../utils/AxiosWithAuth'
 
 const Search = (props) => {
 
-  // useEffect(() => {
-  //   AxiosWithAuth()
-  //     .get('https://wunderlist-be.herokuapp.com/api/v2/todos/2')
-  //     .then(response => {
-  //       console.log(response.data)
-  //     })
-  //     .catch(response => {
-  //       console.log(response)
-  //     })
-  // }, [])
-
-  // useEffect(() => {
-  //   AxiosWithAuth()
-  //     .put('https://wunderlist-be.herokuapp.com/api/v2/todos/2/users/1')
-  //     .then(response => {
-  //       console.log(response.data)
-  //     })
-  //     .catch(response => {
-  //       console.log(response)
-  //     })
-  // }, [])
   console.log('search props :', props)
 
   return (
