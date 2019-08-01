285c3a01d6da4b6d13946bb76b2c9717ac1856db
RealWillBrooks <wbrooks107@gmail.com>
2019-07-31 20:20:05 -0500

    Working on Home.JS, finding best solution to approach sort and mapping/display.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import AxiosWithAuth from '../../utils/AxiosWithAuth';
+
 
 const Home = () => {
   
+const [EverythingState, setEverythingState] = useState([]);
+
+const sortedState = EverythingState.updated_at.sort();
+
+useEffect(() => {
+  AxiosWithAuth()
+      .get(`https://wunderlist-be.herokuapp.com/api/v2/todos`)
+      .then(response => {
+
+        setEverythingState(response.data)
+
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      });
+}, []); 
+
   return(
     <div>
       Home page
@@ -9,4 +29,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
